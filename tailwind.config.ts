45bd055ffb429ff985dcedb7d869ad201e607a5a
yesslee31 <yesslee31@gmail.com>
2025-10-02 19:40:33 +0900

    🔧 config: 서울알림체 폰트 추가 및 메인 색상 변수 Tailwind CSS 설정파일로 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        commuter: "var(--commuter-color)",
       },
       // ios safe area 대응
       spacing: {
