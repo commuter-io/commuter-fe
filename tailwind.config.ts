79ecea0acc77b2210dad42394f33a983ca26b14f
yesslee31 <yesslee31@gmail.com>
2025-11-07 07:03:37 +0900

    📦 build: shadcn/ui 컴포넌트 추가 및 관련 css 코드 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        commuter: "var(--commuter-color)",
+        // commuter: "var(--commuter-color)",
       },
       // ios safe area 대응
       spacing: {
