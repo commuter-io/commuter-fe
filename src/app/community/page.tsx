f9c38e17f9fdd930fab989a08c0230f4f4c91e4a
yesslee31 <yesslee31@gmail.com>
2025-11-07 05:40:23 +0900

    🔨 update: FAB 표시 ClientLayout에서 관리하도록 변경한 점에 관한 수정

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,14 +1,9 @@
 // src/app/community/page.tsx
 
-import CommunityFAB from "@/components/CommunityFAB";
-
 export default function Community() {
 	return (
 		<div className="relative h-full">
 			커뮤니티
-			<div className="fixed inset-x-0 bottom-0 mx-auto h-full max-w-[400px]">
-				<CommunityFAB />
-			</div>
 		</div>
 	);
 }
