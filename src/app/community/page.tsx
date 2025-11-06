98c5c1b1becb4e9b0c56a2f5206642405a4b3cc3
yesslee31 <yesslee31@gmail.com>
2025-11-07 08:01:03 +0900

    ✏️ chore: prettier 규칙 적용

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,9 +1,5 @@
 // src/app/community/page.tsx
 
 export default function Community() {
-	return (
-		<div className="relative h-full">
-			커뮤니티
-		</div>
-	);
+	return <div className="relative h-full">커뮤니티</div>;
 }
