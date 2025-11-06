f1665a5f992b0940d548cdd76f6ba4dfd0a3537d
yesslee31 <yesslee31@gmail.com>
2025-11-06 19:33:04 +0900

    ✨ feat: 커뮤니티 글 작성 FAB 추가

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,5 +1,14 @@
 // src/app/community/page.tsx
 
+import CommunityFAB from "@/components/CommunityFAB";
+
 export default function Community() {
-	return <div>커뮤니티</div>;
+	return (
+		<div className="relative h-full">
+			커뮤니티
+			<div className="fixed inset-x-0 bottom-0 mx-auto h-full max-w-[400px]">
+				<CommunityFAB />
+			</div>
+		</div>
+	);
 }
